8b4450832f0b4edd5a81e6c3df8d945603ebbb23
Sergey <s-kobets617@yandex.ru>
2016-11-21 23:14:42 +0300

    JS lightGallery dinamic

--- static_src/js/main.js
+++ static_src/js/main.js
@@ -1,3 +1,73 @@
 $(document).ready(function() {
-	$("#lightgallery").lightGallery();
+
+	imgClickGalery();
+
+	function imgClickGalery() {
+	    var $active;
+
+	    var $imgs = $('.image');
+
+	    $imgs.on('click', function (event) {
+	        var $this = $(this);
+	        var _this = event.target;
+	        var dynamicArr = [];
+	        var ind = -1;
+
+	        $this.find('img').each(function (index) {
+	            // forming an array
+	            if ($(this).is(_this)) {
+	                ind = index;
+	            }
+	            dynamicArr.push({
+	                'src': $(this).attr('src')
+	            });
+	        });
+
+	        var Galery = $this.lightGallery({
+	            dynamic: true,
+	            dynamicEl: dynamicArr,
+	            enableDrag: false,
+	            index: ind
+	            //enableTouch: false
+	        });
+
+	        Galery.on('onAfterOpen.lg', function () {/*event before Load lightGalery*/
+	            $active = $this;
+	            console.log($active);
+	            $(document)
+	                .on('mouseenter', '.lg-object', function () {
+	                    $(this)
+	                        .closest('.lg')
+	                        .find('.lg-toolbar')
+	                        .addClass('image-hovered');
+	                    $(this)
+	                        .closest('.lg')
+	                        .find('.lg-next')
+	                        .addClass('image-hovered');
+	                })
+	                .on('mouseleave', '.lg-object', function () {
+	                    $(this)
+	                        .closest('.lg')
+	                        .find('.lg-toolbar')
+	                        .removeClass('image-hovered');
+	                    $(this)
+	                        .closest('.lg')
+	                        .find('.lg-next')
+	                        .removeClass('image-hovered');
+	                });
+	        });
+	    });
+
+	    /*click img -> next slide lightGallery*/
+	    $(document).on('click', '.lg-object', function (event) {
+	        $active.data('lightGallery').goToNextSlide();
+	    });
+
+	    /*close lightGallery*/
+	    $(document).on('click', function (event) {
+	        if ($(event.target).hasClass('lg-inner')) {
+	            $active.data('lightGallery').destroy();
+	        }
+	    });
+	}
 });
